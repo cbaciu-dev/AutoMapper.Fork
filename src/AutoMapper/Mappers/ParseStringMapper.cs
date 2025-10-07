3591d65ea1cdb920b0fb8a3a965ed1969bb75f73
cbaciu-dev <ciprian.baciu@tss-yonder.com>
2025-10-07 12:54:34 +0300

    EMZ-3878 Fork from AutoMapper

---
+++ src/AutoMapper/Mappers/ParseStringMapper.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoMapper.Internal.Mappers;
+
+public sealed class ParseStringMapper : IObjectMapper
+{
+    public bool IsMatch(TypePair context) => context.SourceType == typeof(string) && HasParse(context.DestinationType);
+    static bool HasParse(Type type) => type == typeof(Guid) || type == typeof(TimeSpan) || type == typeof(DateTimeOffset);
+    public Expression MapExpression(IGlobalConfiguration configuration, ProfileMap profileMap, MemberMap memberMap, Expression sourceExpression, Expression destExpression) =>
+        Call(destExpression.Type.GetMethod("Parse", [typeof(string)]), sourceExpression);
+#if NETSTANDARD2_0
+    public TypePair? GetAssociatedTypes(TypePair initialTypes) => null;
+#endif
+}
---
+++ src/AutoMapper/Mappers/ToStringMapper.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoMapper.Internal.Mappers;
+public sealed class ToStringMapper : IObjectMapper
+{
+    public bool IsMatch(TypePair context) => context.DestinationType == typeof(string);
+    public Expression MapExpression(IGlobalConfiguration configuration, ProfileMap profileMap, MemberMap memberMap, Expression sourceExpression, Expression destExpression)
+    {
+        var sourceType = sourceExpression.Type;
+        var toStringCall = Call(sourceExpression, ObjectToString);
+        return sourceType.IsEnum ? StringToEnumMapper.CheckEnumMember(sourceExpression, sourceType, toStringCall) : toStringCall;
+    }
+#if NETSTANDARD2_0
+    public TypePair? GetAssociatedTypes(TypePair initialTypes) => null;
+#endif
+}
